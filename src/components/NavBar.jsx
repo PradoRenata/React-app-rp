171a16ec82b63caeae45325df55b2b3328c6250f
Renata Prado <pradov.renata@gmail.com>
2023-11-30 20:28:35 -0300

    Clase 4 check

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import logo from '../assets/dino.svg'
+import { Button } from "./button";
+
+export const NavBar = () => {
+
+    return (
+        <header className="bg-slate-900/90 backdrop-blur drop-shadow-2xl"> 
+            <div className="flex justify-between items-center px-6 py-2">
+                <a href="#" className="text-lg text-white font-bold">DinoShop</a>
+                <nav className="nav__container">
+                    <ul className="flex flex-row">
+                        <li className="text-white text-lg px-6 py-2 hover:text-blue-400"><a href="#"> Productos</a></li>
+                        <li className="text-white text-lg px-6 py-2 hover:text-blue-400"><a href="#"> Categorias</a></li>
+                        <li className="text-white text-lg px-6 py-2 hover:text-blue-400"><a href="#">Compras</a></li>
+                    </ul>
+                </nav>
+                <Button/>
+            </div>
+        </header>
+    )
+}
