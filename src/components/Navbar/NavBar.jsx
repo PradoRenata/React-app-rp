45298da79e4a02c004642325087910266b94fd4f
Renata Prado <pradov.renata@gmail.com>
2023-12-18 19:49:12 -0300

    enlace logo a inicio

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
     return (
         <header className="m-2 bg-slate-900/90 backdrop-blur drop-shadow-2xl rounded-lg sticky top-1 z-50"> 
             <div className="flex justify-between items-center px-6 py-2">
-                <a href="#" className="text-lg font-bold text-white font-bold">DinoShop</a>
+                <a href="/" className="text-lg font-bold text-white font-bold">DinoShop</a>
                 <nav className="flex flex-row">
                     {links.map((link) => (
                 
