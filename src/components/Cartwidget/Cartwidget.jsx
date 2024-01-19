edc37aae515968c82317a26e0ac0af92dabe752d
Renata Prado <pradov.renata@gmail.com>
2024-01-19 14:39:01 -0300

    Arreglos css navbar

--- src/components/Cartwidget/Cartwidget.jsx
+++ src/components/Cartwidget/Cartwidget.jsx
@@ -3,7 +3,7 @@ import { ShoppingCart } from 'react-feather';
 const Cartwidget = () => {
 
     return (
-        <button className="relative px-5 py-3 border border-slate-200 rounded-lg"> <ShoppingCart color='#1F2937' size="24" /> <span className="bg-red-500 px-2 py-1 rounded-full text-white absolute -top-2 -right-2 text-xs">4</span> </button>
+        <button className="relative px-5 py-3 border border-slate-200 rounded-lg"> <ShoppingCart color='#1F2937' size="20" /> <span className="bg-red-500 px-2 py-1 rounded-full text-white absolute -top-2 -right-2 text-xs">4</span> </button>
     )
 }
 
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -30,17 +30,17 @@ const NavBar = () => {
             <div className="flex justify-between px-12 py-3">
               <div className="flex gap-2">
                 <img src={ Isotype } alt="DinoTienda"/>
-                <a href="/" className="font-['Titan_One'] text-xl text-slate-900 leading-5 pt-2">Dino Tienda</a>
+                <a href="/" className="font-['Titan_One'] text-xl text-slate-900 leading-5 pt-2">Dino<br/>Tienda</a>
               </div>
-              <div className='flex gap-4'>
-                 <button className='px-5 py-2 bg-slate-100 rounded-lg'><Sun color='#1F2937' size="24" /></button>
+              <div className='flex gap-2'>
+                 <button className='px-5 py-2 bg-slate-100 rounded-lg'><Sun color='#1F2937' size="20" /></button>
                  <Cartwidget />
-                 <button className='px-5 py-2 border border-slate-200 rounded-lg text-slate-900 text-xl font-medium hover:text-blue-600'>Iniciar sesión</button>
+                 <button className='px-5 py-2 border border-slate-200 rounded-lg text-slate-900 font-medium hover:text-blue-600'>Iniciar sesión</button>
               </div>
             </div>
             <nav className="bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-row justify-center">
                     {links.map((link) => (
-                        <Link key={link.href} to={link.href} className="text-white text-xl font-medium px-5 py-3 border-b-4 border-transparent hover:border-b-4 hover:border-white"> {link.label} </Link>
+                        <Link key={link.href} to={link.href} className="text-white text- px-5 pb-3 pt-4 border-b-4 border-transparent hover:border-b-4 hover:border-white"> {link.label} </Link>
                     ))}
                 </nav>
         </header>
