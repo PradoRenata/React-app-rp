be553f7f7fc26f5966b0082fa8b04a12d4d2e726
Renata Prado <pradov.renata@gmail.com>
2024-01-22 13:05:10 -0300

    emptyCartWidget

--- src/components/Cartwidget/Cartwidget.jsx
+++ src/components/Cartwidget/Cartwidget.jsx
@@ -8,7 +8,7 @@ const Cartwidget = () => {
     const { itemsInCart } = useContext(CartContext)
 
     return (
-        <Link to="/Cart" className="relative px-5 py-3 border border-slate-200 rounded-lg"> <ShoppingCart color='#1F2937' size="20" /> <span className="bg-red-500 px-2 py-1 rounded-full text-white absolute -top-2 -right-2 text-xs">{ itemsInCart() }</span> </Link>
+        <Link to="/Cart" className="relative px-5 py-3 border border-slate-200 rounded-lg"> <ShoppingCart color='#1F2937' size="20" /> <span className={`${itemsInCart() === 0 ? 'invisible opacity-0' : 'visible opacity-100'} bg-red-500 w-6 text-center py-1 rounded-full text-white absolute -top-2 -right-2 text-xs`}>{ itemsInCart() }</span> </Link>
     )
 }
 
